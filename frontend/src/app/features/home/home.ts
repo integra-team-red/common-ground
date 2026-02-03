471d6b21efab78c7220b84e9470ef4a51bd77f81
Leo Mihalcea <leomihalcea@gmail.com>
2026-02-03 22:33:53 +0200

    feat: add generic layout, primeng and i18n

---
+++ frontend/src/app/features/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    Button
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
---
+++ frontend/src/app/shared/components/generic-button/generic-button.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-generic-button',
+  imports: [
+    Button
+  ],
+  templateUrl: './generic-button.html',
+  styleUrl: './generic-button.css',
+})
+export class GenericButton {
+
+}
