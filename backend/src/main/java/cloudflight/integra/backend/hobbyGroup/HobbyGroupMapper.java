fec4c53dde4be37cadcb0044a43e4450151efcba
Emma Vasii <109278650+evel4net@users.noreply.github.com>
2026-02-17 15:59:10 +0200

    feat(JPA): implemented many-to-many Tags for Groups and Users
    Implemented many-to-many:
    - created migration script for join tables group_tags and user_tags
    - added @ManyToMany annotation for linking HobbyGroup and User to Tag
    - modified HobbyGroup and User to include List of Tags and HobbyGroupDto and UserDto to include List of Tag Ids
    - added endpoints in AuthController for setting and adding tags to a user
    
    Refs: #22

--- backend/src/main/java/cloudflight/integra/backend/hobbyGroup/HobbyGroupMapper.java
+++ backend/src/main/java/cloudflight/integra/backend/hobbyGroup/HobbyGroupMapper.java
@@ -2,17 +2,20 @@ package cloudflight.integra.backend.hobbyGroup;
 
 import cloudflight.integra.backend.hobbyGroup.model.HobbyGroup;
 import cloudflight.integra.backend.hobbyGroup.model.HobbyGroupDto;
+import cloudflight.integra.backend.tag.model.Tag;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class HobbyGroupMapper {
 
-    public HobbyGroupDto toDto(HobbyGroup group) {
-        return new HobbyGroupDto(group.getId(), group.getName(), group.getDescription(), group.getRadiusKm());
+    public HobbyGroupDto toDto(HobbyGroup group, List<Long> tagIds) {
+        return new HobbyGroupDto(group.getId(), group.getName(), group.getDescription(), group.getRadiusKm(), tagIds);
     }
 
-    public HobbyGroup toEntity(HobbyGroupDto groupDto) {
-        return new HobbyGroup(groupDto.id(), groupDto.name(), groupDto.description(), groupDto.radiusKm());
+    public HobbyGroup toEntity(HobbyGroupDto groupDto, List<Tag> tags) {
+        return new HobbyGroup(groupDto.id(), groupDto.name(), groupDto.description(), groupDto.radiusKm(), tags);
     }
 
 }
