a7e536b251745de03d01495e58c08ef912cec117
alle13 <132494347+alle13@users.noreply.github.com>
2026-02-17 11:17:36 +0200

    feat(SystemFeedback): persisted SystemFeedback to PostgreSQL -added the @Entity tag to the SystemFeedback -modified the repository to extend the JPA Repository Refs: #10

--- backend/src/main/java/cloudflight/integra/backend/systemFeedback/model/SystemFeedback.java
+++ backend/src/main/java/cloudflight/integra/backend/systemFeedback/model/SystemFeedback.java
@@ -1,9 +1,17 @@
 package cloudflight.integra.backend.systemFeedback.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
 public class SystemFeedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String email;
     private String message;
