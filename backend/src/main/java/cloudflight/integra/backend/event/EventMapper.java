4d8ed89f288cc46c13eab65153ea4317993b2908
Patricia Mateica <patriciamateica33@gmail.com>
2026-02-17 14:30:20 +0200

    feat(JPA): link Event to Location
    - altered the Event table so that it includes a foreign key constraint that references the location id
    - added a new attribute to the Event model, location, which has a ManyToOne relationship in the Event entity and points to Location
    - updated EventDTO and now it includes the uuid locationId field
    - added a new method that finds all the events by the location id and verified that it works correctly
    
    Refs: #21

--- backend/src/main/java/cloudflight/integra/backend/event/EventMapper.java
+++ backend/src/main/java/cloudflight/integra/backend/event/EventMapper.java
@@ -2,6 +2,7 @@ package cloudflight.integra.backend.event;
 
 import cloudflight.integra.backend.event.model.EventDto;
 import cloudflight.integra.backend.event.model.Event;
+import cloudflight.integra.backend.location.model.Location;
 import cloudflight.integra.backend.exceptions.ObjectNotFoundException;
 import cloudflight.integra.backend.hobbyGroup.model.HobbyGroup;
 import org.springframework.stereotype.Component;
@@ -10,17 +11,13 @@ import java.util.Optional;
 
 @Component
 public class EventMapper {
-    public EventDto toDto(Event event) {
-        return new EventDto(event.getId(),
-            event.getTitle(),
-            event.getStartTime(),
-            event.getEndTime(),
-            event.getHobbyGroup().getId());
+    public EventDto toDto(Event event){
+        return new EventDto(event.getId(), event.getTitle(), event.getStartTime(), event.getEndTime(), event.getLocation().getId(), event.getLocation().getId());
     }
 
-    public Event toEntity(EventDto dto, Optional<HobbyGroup> hobbyGroup) {
+    public Event toEntity(EventDto dto, Location location, Optional<HobbyGroup> hobbyGroup) {
         if (hobbyGroup.isEmpty())
             throw new ObjectNotFoundException("Hobby group was not found");
-        return new Event(dto.id(), dto.title(), dto.startTime(), dto.endTime(), hobbyGroup.get());
+        return new Event(dto.id(), dto.title(), dto.startTime(), dto.endTime(), location, hobbyGroup.get());
     }
 }
--- backend/src/main/java/cloudflight/integra/backend/event/model/Event.java
+++ backend/src/main/java/cloudflight/integra/backend/event/model/Event.java
@@ -1,7 +1,8 @@
 package cloudflight.integra.backend.event.model;
 
-import cloudflight.integra.backend.hobbyGroup.model.HobbyGroup;
+import cloudflight.integra.backend.location.model.Location;
 import jakarta.persistence.*;
+import cloudflight.integra.backend.hobbyGroup.model.HobbyGroup;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -14,44 +15,62 @@ public class Event {
     private String title;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
+    @ManyToOne
+    private Location location;
 
     @ManyToOne
     @JoinColumn(name="hobby_group_id", nullable = false)
     private HobbyGroup hobbyGroup;
 
 
-    public Event(UUID id, String title, LocalDateTime startTime, LocalDateTime endTime, HobbyGroup hobbyGroup) {
+    public Event(UUID id, String title, LocalDateTime startTime, LocalDateTime endTime, Location location, HobbyGroup hobbyGroup) {
         this.id = id;
         this.title = title;
         this.startTime = startTime;
         this.endTime = endTime;
         this.hobbyGroup = hobbyGroup;
+        this.location = location;
     }
     public Event(){}
 
     public UUID getId() {
         return id;
     }
+
     public void setId(UUID id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public LocalDateTime getStartTime() {
         return startTime;
     }
+
     public void setStartTime(LocalDateTime startTime) {
         this.startTime = startTime;
     }
+
     public LocalDateTime getEndTime() {
         return endTime;
     }
+
     public void setEndTime(LocalDateTime endTime) {
         this.endTime = endTime;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 
     public HobbyGroup getHobbyGroup() {
