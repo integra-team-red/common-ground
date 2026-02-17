b2d474eec36627a6e7f447b5d643bd73eeb44ed0
Dummage <mtnscu@gmail.com>
2026-02-17 15:46:12 +0200

    feat(SEC): Authentication Using Jwt and User
    Refs: #18
    
    feat(SEC): Complete Authentication and Tutorial File
    
    Refs: #18
    
    feat(SEC): Changed User and UserService
    
    -needed fixing as to not have circular dependency
    Refs: #18
    
    feat(SEC): Complete AuthController Implementation
    
    -needs testing
    Refs: #18
    
    feat(SEC): Finished UserRepository and UserService
    
    -also added JwtFilter and JwtService
    Refs: #18
    
    feat(SEC): Implemented Some Security Functionality
    
    Refs: #18

---
+++ backend/src/main/java/cloudflight/integra/backend/security/SecurityBeansConfig.java
@@ -0,0 +1,37 @@
+package cloudflight.integra.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityBeansConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ backend/src/main/java/cloudflight/integra/backend/user/model/User.java
@@ -0,0 +1,61 @@
+package cloudflight.integra.backend.user.model;
+
+import cloudflight.integra.backend.user.Role;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User() {}
+
+    public User(UUID id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
