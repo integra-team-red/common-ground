df4a92e309fe5a5214bf288862a739662eaa285c
Emma Vasii <109278650+evel4net@users.noreply.github.com>
2026-02-17 20:53:00 +0200

    feat(Tag): added pagination for Tag
    Added pagination for Tag:
    - updated TagController to return Page<TagDto>
    - default sorting by label ascending
    
    Refs: #25

--- backend/src/main/java/cloudflight/integra/backend/tag/TagRepository.java
+++ backend/src/main/java/cloudflight/integra/backend/tag/TagRepository.java
@@ -1,13 +1,13 @@
 package cloudflight.integra.backend.tag;
 
 import cloudflight.integra.backend.tag.model.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    List<Tag> findByNormalizedLabel(String normalizedLabel);
+    Page<Tag> findByNormalizedLabel(String normalizedLabel, Pageable pageable);
 }
