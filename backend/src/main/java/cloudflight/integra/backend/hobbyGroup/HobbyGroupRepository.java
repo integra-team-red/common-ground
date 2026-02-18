d14a7f12812625c6a5f80de7b2b100d3b0f81b43
Violeta-16 <ungureanuvioletamihaela@gmail.com>
2026-02-18 21:52:13 +0200

    feat(hobbyGroup): added pagination to the get endpoints
    I modified the get methods in the controller (apart from the getByID) to accept a Pageable as parameter and return a Page of entities. I also modified the get functions in the service to return pages of hobbyGroupDTOs and added a mapper to the service. I also modified the filter function in the service so it moves the logic to the repository
    
    Refs: #23

--- backend/src/main/java/cloudflight/integra/backend/hobbyGroup/HobbyGroupRepository.java
+++ backend/src/main/java/cloudflight/integra/backend/hobbyGroup/HobbyGroupRepository.java
@@ -1,11 +1,13 @@
 package cloudflight.integra.backend.hobbyGroup;
 
 import cloudflight.integra.backend.hobbyGroup.model.HobbyGroup;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
 public interface HobbyGroupRepository extends JpaRepository<HobbyGroup, UUID> {
-
+    Page<HobbyGroup> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
