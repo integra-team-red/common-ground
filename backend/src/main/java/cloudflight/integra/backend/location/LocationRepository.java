b39655ac75da8e439348ed22b058af40a5b38b41
Dummage <mtnscu@gmail.com>
2026-02-23 14:47:50 +0200

    feat(Location): Pageable Location
    -added pagination for Location getAll and search
    
    Refs: #26

--- backend/src/main/java/cloudflight/integra/backend/location/LocationRepository.java
+++ backend/src/main/java/cloudflight/integra/backend/location/LocationRepository.java
@@ -1,10 +1,13 @@
 package cloudflight.integra.backend.location;
 
 import cloudflight.integra.backend.location.model.Location;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface LocationRepository extends JpaRepository<Location, UUID> {
+    Page<Location> findByNameContainingIgnoreCase(String name, Pageable pageable);
 
 }
