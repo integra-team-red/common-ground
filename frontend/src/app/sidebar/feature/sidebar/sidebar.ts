155f9225b65685d6af7c6359361f6a4a2ba52f69
Leo Mihalcea <leomihalcea@gmail.com>
2026-03-01 15:25:15 +0200

    style: sidebar mobile version

--- frontend/src/app/sidebar/feature/sidebar/sidebar.ts
+++ frontend/src/app/sidebar/feature/sidebar/sidebar.ts
@@ -1,15 +1,18 @@
 import {Component, inject} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import {CommonModule} from "@angular/common";
+import {Button} from "primeng/button";
+import {Drawer} from "primeng/drawer";
 
 @Component({
     selector: 'app-sidebar',
-    imports: [CommonModule, RouterLink, RouterLinkActive],
+    imports: [CommonModule, RouterLink, RouterLinkActive, Button, Drawer],
     templateUrl: './sidebar.html',
     styleUrl: './sidebar.css',
 })
 export class Sidebar {
     private router = inject(Router);
+    visible: boolean = false;
 
     public navRoutes = this.router.config
         .map(route => ({
