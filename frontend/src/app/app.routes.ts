da16e76d6a4a4df8cc3fa4acbe9a463d8e90ed09
Dummage <mtnscu@gmail.com>
2026-03-04 17:21:58 +0200

    fix(FE): Home Needs Stylesheet
    Corrected the home-page.ts to use a stylehseet and updated route.
    
    Refs: #64

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import {Routes} from '@angular/router';
 import {ThemeShowcaseComponent} from "./theme/feature/theme-showcase/theme-showcase";
-import {Home} from "./home/feature/home";
+import {HomePage} from "./home/feature/home-page/home-page";
+
 
 export const routes: Routes = [
     {
         path: '',
         title: "Home",
-        component: Home
+        component: HomePage
     },
     {
         path: 'theme',
