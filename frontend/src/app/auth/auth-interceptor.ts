79c023f426327eda085f82e44edf0f19c9d80a7c
Patricia Mateica <patriciamateica33@gmail.com>
2026-04-22 17:50:06 +0300

    security(FE): Error Interceptor to Handle 401/403
    - user is directed on login page on any 401 error
    - user is directed on login page on any 403 error
    - the changes were made in auth-interceptor on the frontend
    
    Refs: #142

--- frontend/src/app/auth/auth-interceptor.ts
+++ frontend/src/app/auth/auth-interceptor.ts
@@ -1,11 +1,18 @@
-import {HttpInterceptorFn} from "@angular/common/http";
+import {HttpErrorResponse, HttpInterceptorFn} from "@angular/common/http";
+import {inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {catchError, throwError} from 'rxjs';
+import {ToastService} from '../toast-service/toast-service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+    const router = inject(Router);
+    const toastService = inject(ToastService);
+
     const token = document.cookie
         .split('; ')
         .find(row => row.startsWith('jwt='))
         ?.split('=')[1]
-        .trim();
+        ?.trim();
 
     if (token) {
         req = req.clone({
@@ -15,5 +22,14 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
         });
     }
 
-    return next(req);
+    return next(req).pipe(
+        catchError((error: HttpErrorResponse) => {
+            if (error.status === 401 || error.status === 403) {
+                toastService.showError("Your session has expired or you do not" +
+                    " have permission to access this page. Please log in again.");
+                router.navigate(['/login']);
+            }
+            return throwError(() => error);
+        })
+    );
 };
